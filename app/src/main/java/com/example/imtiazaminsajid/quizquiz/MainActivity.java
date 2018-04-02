955cf74dee42b14e15a7d527447f673227ff147b
imtiazaminsajid <ambition.sajid@gmail.com>
2018-04-03 00:11:17 +0600

    Signup and SignIn

--- app/src/main/java/com/example/imtiazaminsajid/quizquiz/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/quizquiz/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.imtiazaminsajid.quizquiz;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialEditText edtNewUser, edtNewPassword, edtNewEmail; // Its for Sign Up
+    MaterialEditText edtUser, edtPassword; // Its for Log in
+
+    Button btnSignUp, btnSignIn;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        btnSignIn = findViewById(R.id.btn_sign_in);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+
+        edtUser = findViewById(R.id.edtUser);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        edtNewUser = findViewById(R.id.edtNewUserName);
+        edtNewPassword = findViewById(R.id.edtNewPassword);
+        edtNewEmail = findViewById(R.id.edtNewEmail);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignUpDialog();
+            }
+        });
+    }
+
+    private void showSignUpDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please fill full all information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+
     }
 }

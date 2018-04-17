8fc615a3ea1b2a174769dd9e85e659e6bc568948
imtiazaminsajid <ambition.sajid@gmail.com>
2018-04-17 12:58:06 +0600

    Category Layout

--- app/src/main/java/com/example/imtiazaminsajid/quizquiz/Interface/ItemClickListener.java
+++ app/src/main/java/com/example/imtiazaminsajid/quizquiz/Interface/ItemClickListener.java
@@ -1,8 +1,11 @@
 package com.example.imtiazaminsajid.quizquiz.Interface;
 
+import android.view.View;
+
 /**
  * Created by Imtiaz Amin Sajid on 4/13/2018.
  */
 
 public interface ItemClickListener {
+    void onClick(View view,int position, boolean isLongClick);
 }
--- app/src/main/java/com/example/imtiazaminsajid/quizquiz/ViewHolder/CategoryViewHolder.java
+++ app/src/main/java/com/example/imtiazaminsajid/quizquiz/ViewHolder/CategoryViewHolder.java
@@ -1,8 +1,20 @@
 package com.example.imtiazaminsajid.quizquiz.ViewHolder;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 /**
  * Created by Imtiaz Amin Sajid on 4/13/2018.
  */
 
-public class CategoryViewHolder {
+public class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public CategoryViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }

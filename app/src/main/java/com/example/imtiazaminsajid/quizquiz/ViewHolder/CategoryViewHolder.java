1a08887b1c616412cea98e387d6ddb855c4f798d
imtiazaminsajid <ambition.sajid@gmail.com>
2018-04-17 14:00:58 +0600

    picasso and firebaseUI

--- app/src/main/java/com/example/imtiazaminsajid/quizquiz/ViewHolder/CategoryViewHolder.java
+++ app/src/main/java/com/example/imtiazaminsajid/quizquiz/ViewHolder/CategoryViewHolder.java
@@ -2,6 +2,11 @@ package com.example.imtiazaminsajid.quizquiz.ViewHolder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.imtiazaminsajid.quizquiz.Interface.ItemClickListener;
+import com.example.imtiazaminsajid.quizquiz.R;
 
 /**
  * Created by Imtiaz Amin Sajid on 4/13/2018.
@@ -9,12 +14,28 @@ import android.view.View;
 
 public class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+    public TextView category_name;
+    public ImageView category_image;
+
+    private ItemClickListener itemClickListener;
+
     public CategoryViewHolder(View itemView) {
         super(itemView);
+        category_image = itemView.findViewById(R.id.category_image);
+        category_name = itemView.findViewById(R.id.category_name);
+
+        itemView.setOnClickListener(this);
+
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
     }
 
     @Override
     public void onClick(View view) {
+
+        itemClickListener.onClick(view, getAdapterPosition(),false);
 
     }
 }
